4afbd0489621f6b3a97600bfa0cda2bb1f048b17
Adam Sawicki <@>
2023-02-03 21:27:54 +0100

    Added links between nodes

--- Project/assets/Scripts/GameMain.ts
+++ Project/assets/Scripts/GameMain.ts
@@ -1,9 +1,15 @@
-import { _decorator, Component, Node, EventMouse, Prefab, instantiate, Vec3, Vec2, v3, Canvas, UITransform } from 'cc';
+import { _decorator, Component, Node, EventMouse, Prefab, instantiate, Vec3, Vec2, v3,
+    Canvas, UITransform, Size, Quat, quat } from 'cc';
 const { ccclass, property } = _decorator;
+
+const RAD_TO_DEG = 180 / Math.PI;
 
 @ccclass('GameMain')
 export class GameMain extends Component {
     @property(Prefab) NodeLPrefab: Prefab;
+    @property(Prefab) LinkLPrefab: Prefab;
+
+    #previousNode: Node = null;
 
     start() {
         console.log('Hello World!');
@@ -20,6 +26,9 @@ export class GameMain extends Component {
         //newNode.setPosition(this.MouseLocationToNodePosition(e.getLocation())); 
         newNode.setPosition(this.MouseLocationToNodePosition(e.getUILocation())); 
         this.node.addChild(newNode);
+        if(this.#previousNode)
+            this.CreateLink(this.#previousNode, newNode);
+        this.#previousNode = newNode;
     }
 
     private MouseLocationToNodePosition(v: Vec2): Vec3
@@ -27,5 +36,29 @@ export class GameMain extends Component {
         let contentSize = this.node.getComponent(UITransform).contentSize;
         return v3(v.x - contentSize.width / 2,
             v.y - contentSize.height / 2);
+    }
+
+    private CreateLink(nodeA: Node, nodeB: Node): void
+    {
+        let linkNode = instantiate(this.LinkLPrefab);
+
+        let posA = nodeA.getPosition();
+        let posB = nodeB.getPosition();
+        let vecAB = v3(); Vec3.subtract(vecAB, posB, posA);
+
+        let midPos = v3(); Vec3.add(midPos, posA, posB);
+        Vec3.divide(midPos, midPos, v3(2, 2, 2));
+        linkNode.setPosition(midPos);
+
+        let width = Vec3.len(vecAB) - 16;
+        let height = linkNode.getComponent(UITransform).contentSize.height;
+        linkNode.getComponent(UITransform).setContentSize(width, height);
+
+        let angle = Math.atan2(vecAB.y, vecAB.x) * RAD_TO_DEG;
+        let rotation = quat();
+        Quat.fromAngleZ(rotation, angle);
+        linkNode.setRotation(rotation);
+
+        this.node.getChildByName('LinksParent').addChild(linkNode);
     }
 }
