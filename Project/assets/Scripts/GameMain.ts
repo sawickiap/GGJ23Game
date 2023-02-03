df0e601513b957af3cf7cda4dbf29ba2d9efaed7
Adam Sawicki <@>
2023-02-03 21:52:03 +0100

    Coded more logic of links

--- Project/assets/Scripts/GameMain.ts
+++ Project/assets/Scripts/GameMain.ts
@@ -1,11 +1,12 @@
 import { _decorator, Component, Node, EventMouse, Prefab, instantiate, Vec3, Vec2, v3,
     Canvas, UITransform, Size, Quat, quat } from 'cc';
 import { NodeScript } from './NodeScript';
+import { LinkScript } from './LinkScript';
 const { ccclass, property } = _decorator;
 
 const RAD_TO_DEG = 180 / Math.PI;
-const NODE_DIST_MIN = 24;
-const NODE_DIST_MAX = 128;
+const NODE_DIST_MIN = 48;
+const NODE_DIST_MAX = 96;
 
 @ccclass('GameMain')
 export class GameMain extends Component {
@@ -45,29 +46,35 @@ export class GameMain extends Component {
 
         let pos = this.MouseLocationToNodePosition(e.getUILocation());
 
-        let closestNodeDist = Number.MAX_VALUE;
+        let tooClose = false;
+        let nearbyNodes: Node[] = [];
         for(let node of this.#leftNodes)
         {
             let dist = Vec3.distance(pos, node.getPosition());
-            if(dist < closestNodeDist)
-                closestNodeDist = dist;
+            if(dist < NODE_DIST_MIN)
+            {
+                tooClose = true;
+                break;
+            }
+            else if(dist <= NODE_DIST_MAX)
+                nearbyNodes.push(node);
         }
-        if(closestNodeDist >= NODE_DIST_MIN &&
-            closestNodeDist <= NODE_DIST_MAX)
+        if(!tooClose && nearbyNodes.length > 0)
         {
-            this.CreateNode(pos);
+            let newNode = this.CreateNode(pos);
+            for(let nearbyNode of nearbyNodes)
+                this.CreateLink(nearbyNode, newNode);
         }
     }
 
-    CreateNode(pos: Vec3): void
+    CreateNode(pos: Vec3): Node
     {
         let newNode = instantiate(this.NodeLPrefab);
         //newNode.setPosition(this.MouseLocationToNodePosition(e.getLocation())); 
         newNode.setPosition(pos); 
-        this.node.addChild(newNode);
-        if(this.#leftNodes.length > 0)
-            this.CreateLink(this.#leftNodes[this.#leftNodes.length - 1], newNode);
+        this.node.addChild(newNode);    
         this.#leftNodes.push(newNode);
+        return newNode;
     }
 
     private MouseLocationToNodePosition(v: Vec2): Vec3
@@ -80,6 +87,10 @@ export class GameMain extends Component {
     private CreateLink(nodeA: Node, nodeB: Node): void
     {
         let linkNode = instantiate(this.LinkLPrefab);
+
+        let linkScript = linkNode.getComponent(LinkScript);
+        linkScript.NodeA = nodeA;
+        linkScript.NodeB = nodeB;
 
         let posA = nodeA.getPosition();
         let posB = nodeB.getPosition();
---
+++ Project/assets/Scripts/LinkScript.ts
@@ -0,0 +1,17 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('LinkScript')
+export class LinkScript extends Component {
+    @property IsLeft: boolean = false;
+    @property NodeA: Node = null;
+    @property NodeB: Node = null;
+
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
